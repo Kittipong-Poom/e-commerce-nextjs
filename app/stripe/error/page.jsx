64aadb58d3f71598103c14b53e0dcfcc6cd12f7c
KITTIPONG WONGSADITYANON <6431503005@lamduan.mfu.ac.th>
2024-08-30 16:24:15 +0700

    test

--- app/stripe/error/page.jsx
+++ app/stripe/error/page.jsx
@@ -1,13 +1,16 @@
 import Link from "next/link";
 
 const ErrorPage = () => {
+  // Get the base URL from environment variables
+  const homeUrl = process.env.NEXT_PUBLIC_HOME_URL || "/";
+
   return (
     <section className="py-72">
       <div className="container mx-auto">
         <h3 className="text-center mb-4">
           Something went wrong!
         </h3>
-        <Link href="/">
+        <Link href={homeUrl}>
           <button className="btn btn-accent mx-auto uppercase rounded-full">
             Back to the homepage
           </button>
